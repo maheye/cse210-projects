931b5663c67b1e301c5514724da391d56b22c8d3
maheye <u277721157@byui.edu>
2023-04-24 16:39:16 +0800

    prep 3 assignment

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -3,7 +3,32 @@ using System;
 class Program
 {
     static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep3 World!");
+    {   
+        Random RandomGenerator = new Random();
+        int number = RandomGenerator.Next(1, 101);
+        Console.WriteLine($"What is the magic number? {number}");
+        int guess = -1;
+        int count = 0;
+        while (guess != number)
+        {
+            Console.Write("What is your guess? ");
+            guess= int.Parse(Console.ReadLine());
+            count += 1;
+
+            if (guess > number)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess < number)
+            {
+                Console.WriteLine("Higher");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+                Console.WriteLine($"It took you {count} time to guess!");
+            }
+        
+        }
     }
 }
